abf7d18bf12bc270c44360b00d848dd0d374fb61
Kyle McColgan <kdmbus21@gmail.com>
2024-05-23 20:16:32 -0500

    Add imports to TestTaskTwo.java and add setter for 'location' field to Employer.java class.

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -16,4 +16,8 @@ public class Employer extends AbstractEntity
     public String getLocation() {
         return location;
     }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
