87491ff36a138177b957d28dce6e4dd77027d530
Kyle McColgan <kdmbus21@gmail.com>
2024-05-23 19:31:05 -0500

    Add 'location' field to Employer.java.

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,15 @@
 package org.launchcode.techjobs.persistent.models;
 
-public class Employer extends AbstractEntity {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class Employer extends AbstractEntity
+{
+    @NotBlank
+    @Size(max=128)
+    private String location;
+
+    public String getLocation() {
+        return location;
+    }
 }
