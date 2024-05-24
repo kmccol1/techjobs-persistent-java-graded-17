996d2b44d0c862566b2d75ac6b8fe9264c509bc8
Kyle McColgan <kdmbus21@gmail.com>
2024-05-23 19:36:21 -0500

    Add Entity annotation to the Skill.java class.

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,13 +1,17 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+@Entity
 public class Skill extends AbstractEntity
 {
     @NotBlank
     @Size(max=128)
     private String description;
+
+    public Skill() {}
 
     public String getDescription() {
         return description;
