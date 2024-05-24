e045dcd3ef308634dbcc607ca95e20fa8e553812
Kyle McColgan <kdmbus21@gmail.com>
2024-05-23 19:33:29 -0500

    Add Entity annotation to Employer.java class.

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,13 +1,17 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+@Entity
 public class Employer extends AbstractEntity
 {
     @NotBlank
     @Size(max=128)
     private String location;
+
+    public Employer(){}
 
     public String getLocation() {
         return location;
