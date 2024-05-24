9a1fb0397adbd5ab50427caf4a77d0af175ed139
Kyle McColgan <kdmbus21@gmail.com>
2024-05-23 19:35:26 -0500

    Add 'description' field to the Skill.java class.

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,15 @@
 package org.launchcode.techjobs.persistent.models;
 
-public class Skill extends AbstractEntity {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
+public class Skill extends AbstractEntity
+{
+    @NotBlank
+    @Size(max=128)
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
 }
